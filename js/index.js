9c251fb16fba638dec6c9bc033bc611629231504
jon doughty <jdoughty@calpoly.edu>
2015-02-03 21:57:09 -0800

    lab 5 updates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function getTemp() {
     var url = "https://api.forecast.io/forecast/a6277e301335db93d68d05955844979f/-33.8674870,151.2069900?callback=?";
 
@@ -29,3 +28,35 @@ function getTemp() {
    
   
 }    
+
+function showAlarmPopup() {
+    $( "#mask" ).removeClass();
+    $( "#popup" ).removeClass();
+} 
+
+function hideAlarmPopup() {
+    $( "#mask" ).addClass("hide");
+    $( "#popup" ).addClass("hide");
+} 
+
+function insertAlarm(hours, mins, ampm, alarmName) {
+    
+    var a = $("<div> </div>").addClass("flexable");
+    
+    var b = $("<div> </div>").addClass("name").html(alarmName);
+   
+    var c = $("<div> </div>").addClass("time").html(hours + ":" + mins + ":" + ampm);
+     
+    a.append(b,c);
+    $("#alarms").append(a);
+    
+} 
+
+function addAlarm() {
+    var hours = $("#hours option:selected").text();
+    var mins = $("#mins option:selected").text();
+    var ampm = $("#ampm option:selected").text();
+    var alarmName = $("#alarmName option:selected").text();
+    insertAlarm(hours, mins, ampm, alarmName);
+    hideAlarmPopup();
+}
