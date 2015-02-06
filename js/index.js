cafea9997b47565a8d995a29cf04d2102b88fde6
jon doughty <jdoughty@calpoly.edu>
2015-02-05 18:06:17 -0800

    parse updates for lab 5

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+function start() {
+    getTemp();
+    getAllAlarms();
+}
+
 function getTemp() {
+    //window.alert ("in getTemp()");
     var url = "https://api.forecast.io/forecast/a6277e301335db93d68d05955844979f/-33.8674870,151.2069900?callback=?";
 
     $.ajax({
@@ -25,9 +31,25 @@ function getTemp() {
                 $( "body" ).addClass("hot");            
         }
    });
-   
-  
+   //window.alert ("exiting getTemp()");
 }    
+
+function getAllAlarms() {
+    //window.alert ("in getAllAlarms()");
+    Parse.initialize("OVhbvBqf5heRgl1rjOiyJlVx60pvulpe8mrvKzeI","0NgvrXuZgZrrpijc6gYsTa8fUTcPIQXsAy1hiaHH");
+    
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    
+    query.find({
+        success: function(results) {
+          for (var i = 0; i < results.length; i++) { 
+            //window.alert( results[i].get("time"));
+            insertAlarm(results[i].get("hours"),results[i].get("mins"), results[i].get("ampm"), results[i].get("alarmName"));
+          }
+        }
+    });   
+}
 
 function showAlarmPopup() {
     $( "#mask" ).removeClass();
@@ -40,23 +62,67 @@ function hideAlarmPopup() {
 } 
 
 function insertAlarm(hours, mins, ampm, alarmName) {
-    
-    var a = $("<div> </div>").addClass("flexable");
-    
+    //window.alert ("insertAlarm()");
+    var a = $("<div>").addClass("flexable");
     var b = $("<div> </div>").addClass("name").html(alarmName);
-   
     var c = $("<div> </div>").addClass("time").html(hours + ":" + mins + ":" + ampm);
-     
     a.append(b,c);
     $("#alarms").append(a);
-    
 } 
 
 function addAlarm() {
+    //window.alert ("addAlarm()");
     var hours = $("#hours option:selected").text();
     var mins = $("#mins option:selected").text();
     var ampm = $("#ampm option:selected").text();
-    var alarmName = $("#alarmName option:selected").text();
-    insertAlarm(hours, mins, ampm, alarmName);
-    hideAlarmPopup();
+    var alarmName = $("#alarmName").val();
+    //window.alert(alarmName);
+    
+    //window.alert("in addAlarm: " + alarmName);
+ 
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new AlarmObject();
+      alarmObject.save({"hours": hours, "mins" : mins, "ampm" : ampm, "alarmName": alarmName}, {
+      success: function(object) {
+        //window.alert("success function");
+        insertAlarm(hours, mins, ampm, alarmName);
+        hideAlarmPopup();
+      }
+    });  
+   
+    
+    //window.alert ("end insertAlarm()");
 }
+
+
+function deleteAlarm() {
+    var name = window.prompt("enter name of alarm to delete", "alarm name");
+    
+    Parse.initialize("OVhbvBqf5heRgl1rjOiyJlVx60pvulpe8mrvKzeI","0NgvrXuZgZrrpijc6gYsTa8fUTcPIQXsAy1hiaHH");
+    
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    
+    query.equalTo("alarmName", name);   
+    query.find({
+        success: function(results) {
+            //window.alert("success");
+            for (var i = 0; i < results.length; i++) { 
+                var object = results[i];
+                object.destroy();
+                //location.reload(true);
+            }
+        },
+        error : function(err) {
+            window.alert(err.message);
+        }
+    });
+   
+    setTimeout(function() {     
+        location.reload(true);
+    }, 200);
+ 
+}
+
+
+
